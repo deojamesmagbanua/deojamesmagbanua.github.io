3bff136de1fe34aac9fbb35941b2eb74b7d65fac
Deo James Magbanua <dmagbanua@se.com>
2020-01-17 11:07:50 +0800

    update

--- crdnl/assets/core/custom/main.js
+++ crdnl/assets/core/custom/main.js
@@ -1,24 +1,24 @@
 $(window).on('load', function () { // makes sure the whole site is loaded 
-    // $(".spinner").css("opacity", "0");
+    $(".spinner").css("opacity", "0");
 
 
-    // function myFunction(x) {
-    //     if (x.matches) { // If media query matches
-    //         setTimeout(() => {
-    //             $("#loading").css("height", "0px");
-    //             $('html, body').removeAttr('style')
-    //         }, 1500);
-    //     } else {
-    //         setTimeout(() => {
-    //             $("#loading").css("width", "0px");
-    //             $('html, body').removeAttr('style')
-    //         }, 1500);
-    //     }
-    // }
+    function myFunction(x) {
+        if (x.matches) { // If media query matches
+            setTimeout(() => {
+                $("#loading").css("height", "0px");
+                $('html, body').removeAttr('style')
+            }, 1500);
+        } else {
+            setTimeout(() => {
+                $("#loading").css("width", "0px");
+                $('html, body').removeAttr('style')
+            }, 1500);
+        }
+    }
 
-    // var x = window.matchMedia("(max-width: 768px)")
-    // myFunction(x) // Call listener function at run time
-    // x.addListener(myFunction) // Attach listener function on state changes
+    var x = window.matchMedia("(max-width: 768px)")
+    myFunction(x) // Call listener function at run time
+    x.addListener(myFunction) // Attach listener function on state changes
     
 
     $("#menutoggle").click(function () {
